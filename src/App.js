401c0c74ca49427bc4b8149dc150eb2ae6dd817d
aykay88 <andreas.kaiser@andrena.de>
2017-04-30 22:29:32 +0200

    initial repo

---
+++ src/App.js
@@ -0,0 +1,41 @@
+import React from 'react';
+
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      txt: "this is the state text",
+      cat: 0
+    }
+  }
+
+  update(event){
+    this.setState({txt: event.target.value})
+
+  }
+
+  render(){
+    return (
+      <div>
+        <input type="text"
+        onChange={this.update.bind(this)}/>
+        <h1>{this.state.txt} - {this.state.cat}</h1>
+      </div>
+    )
+
+
+  }
+}
+
+App.propTypes = {
+  txt: React.PropTypes.string,
+  cat: React.PropTypes.number.isRequired
+}
+
+
+App.defaultProps = {
+  txt: "this is the default txt"
+}
+// const App =()=> <h1> Hello stateless </h1>
+
+export default App
